cfdef58ad09e6b1e4c4340bb1f06b56452957b27
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-03 17:10:00 +0000

    chore: fix

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,12 +5,10 @@ import execa from 'execa'
 export default {
   works: async () =>
     expect(
-      execa.command("node -e ''", {
-        all: true,
-        env: {
-          NODE_OPTIONS: `--require=./src --experimental-loader=${packageName`@dword-design/babel-register-esm`}`,
-        },
-      })
+      execa.command(
+        `node --require=./src --experimental-loader=${packageName`@dword-design/babel-register-esm`} -e ''`,
+        { all: true }
+      )
         |> await
         |> property('all')
     ).toEqual(''),
