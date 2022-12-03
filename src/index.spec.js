c30bec965345e960e7b6529ab9b221cc465155d0
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-03 17:02:49 +0000

    chore: fix

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,23 +1,17 @@
 import { property } from '@dword-design/functions'
 import packageName from 'depcheck-package-name'
 import execa from 'execa'
-import P from 'path'
 
 export default {
-  works: async () => {
-    console.log(JSON.stringify(require.resolve('.')))
-    console.log(JSON.stringify(P.resolve(require.resolve('.'))))
+  works: async () =>
     expect(
       execa.command("node -e ''", {
         all: true,
         env: {
-          NODE_OPTIONS: `--require="${P.resolve(
-            require.resolve('.')
-          )}" --experimental-loader=${packageName`@dword-design/babel-register-esm`}`,
+          NODE_OPTIONS: `--require=./src --experimental-loader=${packageName`@dword-design/babel-register-esm`}`,
         },
       })
         |> await
         |> property('all')
-    ).toEqual('')
-  },
+    ).toEqual(''),
 }
