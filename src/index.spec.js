99306f5b09eb7cb94f39eb85a989ebf9480185b5
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-03 16:47:28 +0000

    chore: fix test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,7 @@
 import { property } from '@dword-design/functions'
 import packageName from 'depcheck-package-name'
 import execa from 'execa'
+import P from 'path'
 
 export default {
   works: async () =>
@@ -8,9 +9,9 @@ export default {
       execa.command("node -e ''", {
         all: true,
         env: {
-          NODE_OPTIONS: `--require="${require.resolve(
+          NODE_OPTIONS: `--require="${P.resolve(require.resolve(
             '.'
-          )}" --experimental-loader=${packageName`@dword-design/babel-register-esm`}`,
+          ))}" --experimental-loader=${packageName`@dword-design/babel-register-esm`}`,
         },
       })
         |> await
