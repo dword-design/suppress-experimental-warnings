e87a5fd706a573fe046bbf5a19ef6b11e40b74a0
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-01-04 20:12:12 +0000

    chore: add failing test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,27 @@
-import { property } from '@dword-design/functions'
+import { endent, property } from '@dword-design/functions'
 import packageName from 'depcheck-package-name'
 import execa from 'execa'
+import outputFiles from 'output-files'
+import withLocalTmpDir from 'with-local-tmp-dir'
 
 export default {
+  module: () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        'entry.js': endent`
+        import '../src/index.js'
+        import './mod.js'
+      `,
+        'foo.json': JSON.stringify({}),
+        'mod.js': "import './foo.json' assert { type: 'json' }",
+        'package.json': JSON.stringify({ type: 'module' }),
+      })
+      expect(
+        execa.command('node entry.js', { all: true })
+          |> await
+          |> property('all')
+      ).toEqual('')
+    }),
   works: async () =>
     expect(
       execa.command(
