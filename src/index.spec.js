79af0e82cffd8ad075b33d3a76c62d239db6cdfb
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-04-09 14:32:45 +0000

    fix: fixes

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,40 +1,41 @@
 import { endent, property } from '@dword-design/functions'
 import packageName from 'depcheck-package-name'
-import execa from 'execa'
+import { execaCommand } from 'execa'
 import outputFiles from 'output-files'
 import withLocalTmpDir from 'with-local-tmp-dir'
 
 const nodeMajorVersion = parseInt(process.versions.node.split('.')[0], 10)
 
 export default {
+  before: () => execaCommand('base prepublishOnly'),
   ...(nodeMajorVersion > 14 && {
     module: () =>
       withLocalTmpDir(async () => {
         // Execution order is non-deterministic (also when imported from node_modules)
         await outputFiles({
           'entry.js': endent`
-          import '../src/index.js'
+            import '../dist/cjs-fallback.cjs'
 
-          await import('./mod.js')
-        `,
+            await import('./mod.js')
+          `,
           'foo.json': JSON.stringify({}),
           'mod.js': "import './foo.json' assert { type: 'json' }",
           'package.json': JSON.stringify({ type: 'module' }),
         })
         expect(
-          execa.command('node entry.js', { all: true })
+          execaCommand('node entry.js', { all: true })
             |> await
-            |> property('all')
+            |> property('all'),
         ).toEqual('')
       }),
   }),
   works: async () =>
     expect(
-      execa.command(
-        `node --require=./src --experimental-loader=${packageName`@dword-design/babel-register-esm`} -e ''`,
-        { all: true }
+      execaCommand(
+        `node --require=. --experimental-loader=${packageName`@dword-design/babel-register-esm`} -e ''`,
+        { all: true },
       )
         |> await
-        |> property('all')
+        |> property('all'),
     ).toEqual(''),
 }
